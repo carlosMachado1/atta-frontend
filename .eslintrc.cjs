e19494da49e19046f96c54b350142557e2a0de9a
Carlos Gabriel da Silva Machado <carlos.machado1980@yahoo.com>
2024-01-14 19:12:05 -0400

    feat: first commit
    created on going project git repository.

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+    root: true,
+    "extends": [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "@vue/eslint-config-typescript"
+    ],
+    parserOptions: {
+        ecmaVersion: "latest"
+    },
+    rules: {
+        "semi": [
+            "error",
+            "always"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "indent": [
+            "error",
+            4
+        ],
+        "array-bracket-spacing": [
+            "error",
+            "always"
+        ],
+        "object-curly-spacing": [
+            "error",
+            "always"
+        ]
+
+    }
+};
